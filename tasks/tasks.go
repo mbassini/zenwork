478dc94a22304558cb4bbb5604bd79258b5c6aca
Mauro <mbassini@outlook.com>
2025-02-17 15:46:34 -0300

    Add new task

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,1 +1,32 @@
 package tasks
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/mbassini/zenwork/model"
+	"github.com/mbassini/zenwork/storage"
+)
+
+// *storage.ListsData -> Access directly to the slice loaded in storage
+
+func New(task model.Task, listID int) error {
+	fmt.Printf("Adding task: %s \n", task.Name)
+
+	found := false
+	for i, l := range *storage.ListsData {
+		if l.ID == listID {
+			task.ID = len(l.Tasks) + 1
+			task.CreatedAt = time.Now()
+			(*storage.ListsData)[i].Tasks = append(l.Tasks, task)
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("could not find a list with id <%d>", listID)
+	}
+
+	return storage.SaveLists()
+}
